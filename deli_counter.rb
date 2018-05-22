20a6089ddf674ebc34f3ccd63a9bdbbb68d1a5a3
Paris Dolfman <parisdolfman@gmail.com>
2018-05-22 01:08:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    katz_deli.shift
+    katz_deli.shift = name
+    puts "Currently serving #{name}."
   end
 end
