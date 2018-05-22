c13163e56a22959bc2139c33b76dbf69a9e71f91
Paris Dolfman <parisdolfman@gmail.com>
2018-05-22 01:00:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,13 +17,6 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
-#  katz_deli.each.with_index(1) do |index|
-#  puts "Welcome, #{name}. You are number #{index} in line."
-
-#  if katz_deli.length == 0
-#    katz_deli << name
-#  else katz_deli.length == 1
-    # shovel a person to end of line
 
 
 def now_serving
