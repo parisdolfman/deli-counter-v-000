aa6630598b4e51389147980cb2ccfa7fddad9df3
Paris Dolfman <parisdolfman@gmail.com>
2018-05-22 00:31:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+#  kat_deli.each do |line_num|
+
+  #true condition ? "#{line_num}" : "false statement to return"
+end
+
+def take_a_number(katz_deli, name)
+
+end
+
+def now_serving
+
+end
