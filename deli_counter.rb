1bb97b010dc68fc65456ce6bb8f4cb9ffaaaa6a5
Paris Dolfman <parisdolfman@gmail.com>
2018-05-22 00:37:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,10 @@ katz_deli = []
 def line(katz_deli)
  if katz_deli.length == 0
    puts "The line is currently empty."
- else "The line is currently:"
+ else 
+   message = "The line is currently:"
+   katz_deli.each.with_index(1) do |name, index|
+     message << " #{index}. #{person}"
  end
 end
 
