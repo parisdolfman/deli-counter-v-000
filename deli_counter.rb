2bff9b827ffb0ad1f47164bdea4cfb8fb6913fa2
Paris Dolfman <parisdolfman@gmail.com>
2018-05-22 01:07:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,10 +19,11 @@ def take_a_number(katz_deli, name)
 end
 
 
-def now_serving(katz_deli)
+def now_serving(katz_deli, name)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
     katz_deli.shift
+    puts "Currently serving #{name}."
   end
 end
